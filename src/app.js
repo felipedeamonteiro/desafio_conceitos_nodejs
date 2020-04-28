5a8b6e6ced0fd52b7dfbd492567682ca23aac464
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-04-28 00:42:05 -0300

    Made CRU routes to aplication

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,15 +12,45 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  if (!(title && url && techs)) {
+    return response.status(400).json({ error: "Fill all the three fields to continue." })
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository){
+    return response.status(400).json({ error: "There is not a repository like this." })
+  }
+
+  const { title, url, techs } = request.body;
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -27,7 +58,17 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const  { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository){
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
